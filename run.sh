59df193c0491778c2dff9cd29bba3e22b53181fb
Santos82h <santos82h@gmail.com>
2015-09-28 20:13:29 +0200

    cambios menores

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ if [ $? -ne 0 ]; then
 fi
 
 cat head.html body.html foot.html > index.html
-wput -nc -u index.html ftp://back.host22.com/public_html/bicis/
+wput -q -nc -u index.html ftp://back.host22.com/public_html/bicis/
